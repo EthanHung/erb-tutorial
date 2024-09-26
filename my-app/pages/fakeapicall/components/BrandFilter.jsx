31aa30a9ec79e3a3141a3c3e6727b6684531a931
Thirteenneway <samliu13@hotmail.com>
2024-09-26 15:02:10 +0800

    sa

--- my-app/pages/fakeapicall/components/BrandFilter.jsx
+++ my-app/pages/fakeapicall/components/BrandFilter.jsx
@@ -1,26 +1,37 @@
 import * as React from 'react';
 import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-
 
 const data = [
     {
         brandName: 'Giorgio',
         iteamCount: 1,
     }
-]
+    {
+        brandName: 'Lacouts',
+        iteamCount: 1,
+    }
+    {
+        brandName: 'Prada',
+        iteamCount: 1,
+    }
+    {
+        brandName: 'Zara',
+        iteamCount: 2,
+    }
+];
 
 const BrandFilter = () => {
     return (
-        <div>
-      <Checkbox {...label} defaultChecked />
-      <Checkbox {...label} />
-      <Checkbox {...label} disabled />
-      <Checkbox {...label} disabled checked />
-    </div>
-    );
-};
-
-export default BrandFilter;
-
+        {
+            data.map ((item, i) => {
+                return  (
+                    FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                label = {`${data[0].brandName ( $data[0].itemCount)}`});
+            )}
+        }
+        )
+    }
+            export default BrandFilter;
